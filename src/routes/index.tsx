bd6c1a2c068b6e4dbd8f6278a3657aece37f9705
dekelgustavo <dekelgustavo@gmail.com>
2022-06-04 12:34:56 -0300

    feat: settings done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/dashboard/Dashboard';
 
 const AppRoutes: React.FC = () => {
 
   return (
       <Routes>
-          <Route path='/main-page' />
+          <Route path='/main-page' element={<Dashboard />}/>
               
           <Route path='*' element={<Navigate to="/main-page" />} />
       </Routes>
